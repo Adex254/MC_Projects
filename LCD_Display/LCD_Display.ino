4e465b97f5330572a9837464a97806ec7d2d7495
Adex254 <aderinsolaademola781@gmail.com>
2026-04-25 05:34:50 +0100

    Create LCD_Display.ino

---
+++ LCD_Display/LCD_Display.ino
@@ -0,0 +1,15 @@
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+// I2C address is usually 0x27. For 16 chars and 2 lines.
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+void setup() {
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0, 0); 
+  lcd.print("Hello ADEOYE");
+}
+
+void loop() {
+}
