b70bf27a0a15f54769aa73de4da0433a58a1014a
Adex254 <aderinsolaademola781@gmail.com>
2026-04-25 10:33:03 +0100

    Update IR_Game.ino

--- IR_Game/IR_Game.ino
+++ IR_Game/IR_Game.ino
@@ -2,47 +2,51 @@
 #include <LiquidCrystal_I2C.h>
 #include <IRremote.hpp>
 
-// If your screen stays blank after this, change 0x27 to 0x3F
-LiquidCrystal_I2C lcd(0x27, 16, 2); 
+// Using 0x27 because it worked in your Scanner code
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 const int IR_RECEIVE_PIN = 15;
 const int buzzerPin = 19;
 
 void setup() {
-  // Forces the ESP32 to use the correct I2C pins
-  Wire.begin(21, 22); 
-  
+  // Setup the LCD exactly like the Scanner
   lcd.init();
   lcd.backlight();
   
   pinMode(buzzerPin, OUTPUT);
   
-  // STARTUP BEEP: If you don't hear this, the flash failed
+  // Beep once to confirm the code is running
   digitalWrite(buzzerPin, HIGH);
-  delay(300);
+  delay(200);
   digitalWrite(buzzerPin, LOW);
 
+  // Start the IR Receiver
   IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK);
-  
+
   lcd.setCursor(0, 0);
-  lcd.print("IR TEST READY");
+  lcd.print("ADEOYE GAME");
+  lcd.setCursor(0, 1);
+  lcd.print("READY...");
 }
 
 void loop() {
   if (IrReceiver.decode()) {
-    // Beep when you press a remote button
+    // Get the hex code
+    uint32_t received = IrReceiver.decodedIRData.decodedRawData;
+
+    // Beep briefly
     digitalWrite(buzzerPin, HIGH);
-    delay(100);
+    delay(50);
     digitalWrite(buzzerPin, LOW);
 
-    uint32_t received = IrReceiver.decodedIRData.decodedRawData;
-    
+    // Update LCD
     lcd.clear();
     lcd.setCursor(0, 0);
-    lcd.print("HEX CODE:");
+    lcd.print("REMOTE CODE:");
     lcd.setCursor(0, 1);
     lcd.print(received, HEX);
-    
+
+    // Resume for next press
     IrReceiver.resume();
   }
 }
