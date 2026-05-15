7b875d5cd5eca992513b79d23734309714f621e2
Adex254 <aderinsolaademola781@gmail.com>
2026-05-15 17:04:04 +0100

    Update lcd_test.ino

--- lcd_test/lcd_test.ino
+++ lcd_test/lcd_test.ino
@@ -1,1 +1,16 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 
+// Set the LCD address to 0x3F for a 16 chars and 2 line display
+LiquidCrystal_I2C lcd(0x3F, 16, 2);
+
+void setup() {
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print("ADEKUNJO");
+}
+
+void loop() {
+  // Static display, nothing needed in loop
+}
